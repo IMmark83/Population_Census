94a69e83baadd3024c4ba227a2462a4cc13fbaa7
MalevichIM <malevichim@mail.ru>
2023-10-08 09:45:24 +0300

    Add minor filter

--- Main.java
+++ Main.java
@@ -1,10 +1,12 @@
 import java.util.*;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
 
         List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
         List<String> families = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown");
+        List<Integer> age = Arrays.asList(15, 55, 45, 36, 19, 18, 27);
         Collection<Person> persons = new ArrayList<>();
         for (int i = 0; i < 10_000_000; i++) {
             persons.add(new Person(
@@ -15,5 +17,12 @@ public class Main {
                     Education.values()[new Random().nextInt(Education.values().length)])
             );
         }
+
+        long count = age.stream()
+                .filter(x -> x < 18)
+                .count();
+        System.out.println("\nВ списке " + count + " несовершеннолетний(х).");
+
+
     }
 }
