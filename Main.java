09eade43a4d0082f5090deffa8de464d20170822
MalevichIM <malevichim@mail.ru>
2023-10-08 13:48:48 +0300

    remodeling minor filter and add recruit filter

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
@@ -6,7 +7,7 @@ public class Main {
 
         List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
         List<String> families = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown");
-        List<Integer> age = Arrays.asList(15, 55, 45, 36, 19, 18, 27);
+        //  List<Integer> age = Arrays.asList(15, 55, 45, 36, 19, 18, 27);
         Collection<Person> persons = new ArrayList<>();
         for (int i = 0; i < 10_000_000; i++) {
             persons.add(new Person(
@@ -17,11 +18,17 @@ public class Main {
                     Education.values()[new Random().nextInt(Education.values().length)])
             );
         }
+        //список несовершеннолетних
+        long minor = persons.stream().filter(person -> person.getAge() < 18).count();
+        System.out.println("\nВ списке " + minor + " несовершеннолетний(х).");
 
-        long count = age.stream()
-                .filter(x -> x < 18)
-                .count();
-        System.out.println("\nВ списке " + count + " несовершеннолетний(х).");
+        //список фамилий призывников(т.е. мужчин от 18 и до 27 лет)
+        List<String> recruit = persons.stream().filter(person -> (person.getAge() >= 18 && person.getAge() <= 27))
+                .filter(person -> person.getSex() == Sex.MAN).map(Person::getFamily)
+                .collect(Collectors.toList());
+        // System.out.println("\nСписок призвников: " + recruit);
+
+        //список потенциально трудоспособных с высшим образованием (М:18-65 лет, Ж:18-60 лет)
 
 
     }
