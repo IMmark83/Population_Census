2e5adeb10749ae07f869c3b4fe9b170c8d7f3f04
MalevichIM <malevichim@mail.ru>
2023-10-09 16:44:20 +0300

    add ableToWork filter

--- Main.java
+++ Main.java
@@ -7,7 +7,6 @@ public class Main {
 
         List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
         List<String> families = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown");
-        //  List<Integer> age = Arrays.asList(15, 55, 45, 36, 19, 18, 27);
         Collection<Person> persons = new ArrayList<>();
         for (int i = 0; i < 10_000_000; i++) {
             persons.add(new Person(
@@ -20,16 +19,26 @@ public class Main {
         }
         //список несовершеннолетних
         long minor = persons.stream().filter(person -> person.getAge() < 18).count();
-        System.out.println("\nВ списке " + minor + " несовершеннолетний(х).");
+
+        //тест
+//        System.out.println("\nВ списке " + minor + " несовершеннолетний(х).");
 
         //список фамилий призывников(т.е. мужчин от 18 и до 27 лет)
         List<String> recruit = persons.stream().filter(person -> (person.getAge() >= 18 && person.getAge() <= 27))
                 .filter(person -> person.getSex() == Sex.MAN).map(Person::getFamily)
                 .collect(Collectors.toList());
-        // System.out.println("\nСписок призвников: " + recruit);
+
+        //тест
+//         System.out.println("\nСписок призвников: " + recruit);
 
         //список потенциально трудоспособных с высшим образованием (М:18-65 лет, Ж:18-60 лет)
+        Collection<Person> ableToWork = persons.stream()
+                .filter(person -> person.getEducation() == Education.HIGHER)
+                .filter(person -> person.getSex() == Sex.MAN ? person.getAge() >= 18
+                        && person.getAge() <= 65 : person.getAge() >= 18 && person.getAge() <= 60)
+                .sorted(Comparator.comparing(Person::getFamily)).collect(Collectors.toList());
 
-
+        //тест
+//        System.out.println(ableToWork);
     }
 }
